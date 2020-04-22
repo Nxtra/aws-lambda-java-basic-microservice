67360b8886b353b9d45853ef5889624576f52389
Nxtra <nickvanhoof1@gmail.com>
2020-04-22 10:32:43 +0200

    create basic working example that you can deploy

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # AWS-Lambda-java-basic-microservice
 
+## Build and deploy
+
+* build: `./gradlew clean build`
+* deploy first time: `sam deploy --guided`
+* deploy: `sam deploy`
--- src/main/java/io/theclouddeveloper/tips/Handler.java
+++ src/main/java/io/theclouddeveloper/tips/Handler.java
@@ -3,12 +3,24 @@ package io.theclouddeveloper.tips;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
 
-public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyRequestEvent> {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Handler implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
 
     @Override
-    public APIGatewayProxyRequestEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
-        return null;
-        
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        String body = input.getBody();
+
+        Map<String, String> headers = Map.of("Content-Type", "application/json");
+
+        APIGatewayProxyResponseEvent response = new APIGatewayProxyResponseEvent()
+//                .withBody(body)
+                .withHeaders(headers)
+                .withStatusCode(200);
+
+        return response;
     }
 }
